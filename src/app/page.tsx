61ac34291685cbb948cca7a0f34a5470a97b6319
Andre <andrezorek@gmail.com>
2025-01-30 14:30:35 -0300

    :fix: alters default page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          //className="dark:invert"
           src="/starty.svg"
           alt="Starty Logo"
           width={180}
@@ -14,16 +13,7 @@ export default function Home() {
         />
         <h1>Starty v0.1</h1>
         <code>Um starter baseado em Next15+ e Tailwind v4+</code>
-        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol> */}
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -39,7 +29,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Documentação Next App Router
         </a>
       </footer>
     </div>
